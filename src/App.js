00209e8620d7c9883e79f85bc645eaf9944e5c1f
gratele <1216653471@qq.com>
2019-09-09 16:35:47 +0800

    react-router react-router-dom react-router-config ...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import { GlobalStyle } from  './style';
-import {IconStyle} from './assets/iconfont/iconfont'
+import { GlobalStyle } from './style';
+import { IconStyle } from './assets/iconfont/iconfont'
+import { renderRoutes } from 'react-router-config';//renderRoutes读取路由配置转化为Route标签
+import routes from './routes/index.js';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <HashRouter>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+      {renderRoutes(routes)}
+    </HashRouter>
   );
 }
 
