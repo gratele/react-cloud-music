f8dcd1a3df398d3d8a378e0ef1ae99fba11131d1
gratele <1216653471@qq.com>
2019-09-17 14:05:54 +0800

    singers component

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -5,41 +5,41 @@ import * as actionTypes from './store/actionCreators';
 import RecommendList from '../../components/list/';
 import Scroll from '../../baseUI/scroll/index';
 import { Content } from './style';
-// import { forceCheck } from 'react-lazyload';
+import { forceCheck } from 'react-lazyload';
 import { renderRoutes } from 'react-router-config';
 // import { EnterLoading } from './../Singers/style';
-// import Loading from '../../baseUI/loading-v2/index';
+import Loading from '../../baseUI/loading-v2/index';
 
-function Recommend(props){
+function Recommend(props) {
   const { bannerList, recommendList, songsCount, enterLoading } = props;
 
   const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
   useEffect(() => {
-    if(!bannerList.size){
+    if (!bannerList.size) {
       getBannerDataDispatch();
     }
-    if(!recommendList.size){
+    if (!recommendList.size) {
       getRecommendListDataDispatch();
     }
     // eslint-disable-next-line
   }, []);
 
   const bannerListJS = bannerList ? bannerList.toJS() : [];
-  const recommendListJS = recommendList ? recommendList.toJS() :[];
+  const recommendListJS = recommendList ? recommendList.toJS() : [];
 
   return (
     <Content play={songsCount}>
-      <Scroll className="list" >
-      {/* <Scroll className="list" onScroll={forceCheck}> */}
+      <Scroll className="list" onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
       {/* {enterLoading? <EnterLoading><Loading></Loading></EnterLoading> : null} */}
-      { renderRoutes(props.route.routes) }
-    </Content> 
+      {enterLoading ? <Loading></Loading> : null}
+      {renderRoutes(props.route.routes)}
+    </Content>
   );
 }
 
@@ -47,7 +47,7 @@ const mapStateToProps = (state) => ({
   bannerList: state.getIn(['recommend', 'bannerList']),
   recommendList: state.getIn(['recommend', 'recommendList']),
   // songsCount: state.getIn(['player', 'playList']).size,
-  // enterLoading: state.getIn(['recommend', 'enterLoading'])
+  enterLoading: state.getIn(['recommend', 'enterLoading'])
 });
 
 const mapDispatchToProps = (dispatch) => {
