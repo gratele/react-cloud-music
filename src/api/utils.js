9a78e84662886e94e73c2fd4027a3f5127d99321
gratele <1216653471@qq.com>
2019-09-20 16:22:59 +0800

    add rank page

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,3 +1,5 @@
+import { RankTypes } from '../api/config'
+
 export const getCount = (count) => {
     if (count < 0) return;
     if (count < 10000) {
@@ -12,12 +14,29 @@ export const getCount = (count) => {
 export const debounce = (func, delay) => {
     let timer;
     return function (...args) {
-      if(timer) {
-        clearTimeout(timer);
-      }
-      timer = setTimeout(() => {
-        func.apply(this, args);
-        clearTimeout(timer);
-      }, delay);
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            func.apply(this, args);
+            clearTimeout(timer);
+        }, delay);
     }
-  }
+}
+
+//处理数据，找出第一个没有歌名的排行榜的索引
+export const filterIndex = rankList => {
+    for (let i = 0; i < rankList.length - 1; i++) {
+        if (rankList[i].tracks.length && !rankList[i + 1].tracks.length) {
+            return i + 1;
+        }
+    }
+};
+
+//找出排行榜的编号
+export const filterIdx = name => {
+    for (let key in RankTypes) {
+        if (RankTypes[key] === name) return key;
+    }
+    return null;
+};
